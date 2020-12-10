1ef7fa2c481bc4cabcd091c3d4d6c9646f99b7a3
ding <1941423895@qq.com>
2020-12-10 12:16:40 +0800

    添加卡片shadow

---
+++ project/Android/TimeStoryPrj/app/src/main/java/net/onest/timestoryprj/customview/CusShadowLayout.java
@@ -0,0 +1,133 @@
+package net.onest.timestoryprj.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.core.content.ContextCompat;
+
+import net.onest.timestoryprj.R;
+
+public class CusShadowLayout extends RelativeLayout {
+
+    public static final int ALL = 0x1111;
+
+    public static final int LEFT = 0x0001;
+
+    public static final int TOP = 0x0010;
+
+    public static final int RIGHT = 0x0100;
+
+    public static final int BOTTOM = 0x1000;
+
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    private RectF mRectF = new RectF();
+
+    private int mShadowColor = Color.TRANSPARENT;
+
+    private float mShadowRadius = 0;
+
+    private float mShadowDx = 0;
+
+    private float mShadowDy = 0;
+
+    private int mShadowSide = ALL;
+
+    public CusShadowLayout(Context context) {
+        this(context, null);
+    }
+
+    public CusShadowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CusShadowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        float effect = mShadowRadius;
+        float rectLeft = 0;
+        float rectTop = 0;
+        float rectRight = this.getWidth();
+        float rectBottom = this.getHeight();
+        int paddingLeft = 0;
+        int paddingTop = 0;
+        int paddingRight = 0;
+        int paddingBottom = 0;
+
+        if (((mShadowSide & LEFT) == LEFT)) {
+            rectLeft = effect;
+            paddingLeft = (int) effect;
+        }
+        if (((mShadowSide & TOP) == TOP)) {
+            rectTop = effect;
+            paddingTop = (int) effect;
+        }
+        if (((mShadowSide & RIGHT) == RIGHT)) {
+            rectRight = this.getWidth() - effect;
+            paddingRight = (int) effect;
+        }
+        if (((mShadowSide & BOTTOM) == BOTTOM)) {
+            rectBottom = this.getHeight() - effect;
+            paddingBottom = (int) effect;
+        }
+        if (mShadowDy != 0.0f) {
+            rectBottom = rectBottom - mShadowDy;
+            paddingBottom = paddingBottom + (int) mShadowDy;
+        }
+        if (mShadowDx != 0.0f) {
+            rectRight = rectRight - mShadowDx;
+            paddingRight = paddingRight + (int) mShadowDx;
+        }
+        mRectF.left = rectLeft;
+        mRectF.top = rectTop;
+        mRectF.right = rectRight;
+        mRectF.bottom = rectBottom;
+        this.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(mRectF, mPaint);
+    }
+
+    private void init(AttributeSet attrs) {
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);  // 关闭硬件加速
+        this.setWillNotDraw(false);                    // 调用此方法后，才会执行 onDraw(Canvas) 方法
+
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ShadowLayout);
+        if (typedArray != null) {
+            mShadowColor = typedArray.getColor(R.styleable.ShadowLayout_shadowColor,
+                    ContextCompat.getColor(getContext(), android.R.color.black));
+            mShadowRadius = typedArray.getDimension(R.styleable.ShadowLayout_shadowRadius, dip2px(0));
+            mShadowDx = typedArray.getDimension(R.styleable.ShadowLayout_shadowDx, dip2px(0));
+            mShadowDy = typedArray.getDimension(R.styleable.ShadowLayout_shadowDy, dip2px(0));
+            mShadowSide = typedArray.getInt(R.styleable.ShadowLayout_shadowSide, ALL);
+            typedArray.recycle();
+        }
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.TRANSPARENT);
+        mPaint.setShadowLayer(mShadowRadius, mShadowDx, mShadowDy, mShadowColor);
+    }
+
+    private float dip2px(float dpValue) {
+        DisplayMetrics dm = getContext().getResources().getDisplayMetrics();
+        float scale = dm.density;
+        return (dpValue * scale + 0.5F);
+    }
+}
+
